54e9b4999bc0a9faf0d6125537a87469ff30b0df
Student8 <Student8@10.8.33.107>
2016-10-18 14:03:33 -0400

    Dice roll statistics

--- Period 8 Java/src/arrays/ArrayAndArrayList.java
+++ Period 8 Java/src/arrays/ArrayAndArrayList.java
@@ -4,38 +4,55 @@ public class ArrayAndArrayList {
 
 	public static void main(String[] args) {
 		int[] fiftyNumbers = new int[50];
-		populate(fiftyNumbers);
-		randomize(fiftyNumbers);
-		print(fiftyNumbers);
-		rollDice(fiftyNumbers);
-		
-		//Counts each die roll and provide a percentage
-		countResult(fiftyNumbers);
+		//populate(fiftyNumbers);
+		//randomize(fiftyNumbers);
+		rollDice(fiftyNumbers, 3);
 	}
 	
-	private static void countResult(int[] fiftyNumbers) {
-		// TODO Auto-generated method stub
-		
+	private static void countResult(int[] array, int numberOfDice) {
+		//Counts each die roll and provide a percentage
+		int[] counter = new int[numberOfDice * 6];
+		for(int n: array){
+			counter[n-1]++;
+		}
+		for(int i = numberOfDice; i < counter.length; i++){
+			System.out.println((i+1) + " was rolled " + 100 * counter[i] / array.length+ " percent of the time");
+		}
 	}
 
-	private static void rollDice(int[] fiftyNumbers) {
-		// TODO Auto-generated method stub
-		
+	private static void rollDice(int[] array, int numberOfDice) {
+		for(int i = 0; i < array.length; i++){
+			int sum = 0;
+			for(int j = 0; j < numberOfDice; j++){
+				sum += randomInteger(6)+1;							
+			}
+			array[i] = sum;
+		}
+		countResult(array, numberOfDice);
 	}
 
-	private static void print(int[] fiftyNumbers) {
-		// TODO Auto-generated method stub
-		
+	private static void print(int[] array) {
+		for(int i = 0; i < array.length; i++){
+			System.out.println(array[i]);
+		}
 	}
 
-	private static void randomize(int[] fiftyNumbers) {
-		// TODO Auto-generated method stub
-		
+	private static void randomize(int[] array) {
+		for(int i = 0; i < 50; i++){
+			array[i] = randomInteger(25);
+		}
+		print(array);
 	}
 
-	private static void populate(int[] fiftyNumbers) {
-		// TODO Auto-generated method stub
-		
+	private static void populate(int[] array) {
+		for(int i = 0; i < 50; i++){
+			array[i] = i + 1;
+		}
+		print(array);
 	}
 
+	private static int randomInteger(int max){
+		//Returns a integer from [0,end)
+		return (int)(Math.random() * max);
+	}
 }
