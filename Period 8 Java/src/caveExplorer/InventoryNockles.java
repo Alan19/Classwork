6bc998cd622f4f2a4be93b9dcab510b526c0a590
Student8 <Student8@10.8.33.107>
2016-11-01 14:04:44 -0400

    Added inventories, connections and doors

--- Period 8 Java/src/caveExplorer/InventoryNockles.java
+++ Period 8 Java/src/caveExplorer/InventoryNockles.java
@@ -1,5 +1,23 @@
 package caveExplorer;
 
 public class InventoryNockles {
+	private boolean hasMap;
+	private String map;
+	
+	public InventoryNockles() {
+		hasMap = true;
+		updateMap();
+	}
+
+	private void updateMap() {
+		CaveRoomPd8[][] caves = CaveExplorer.caves;
+		//Convert these caves to a string representation
+		
+	}
+
+	public String getDescription() {
+		if(hasMap) return map;
+		else return "You have no inventory";
+	}
 
 }
--- Period 8 Java/src/caveExplorer/TwoDArraysIntro.java
+++ Period 8 Java/src/caveExplorer/TwoDArraysIntro.java
@@ -8,22 +8,46 @@ public class TwoDArraysIntro {
 //		String[] xox = {"x", "o", "x", "o", "x", "o", "x"};
 //		System.out.println(Arrays.toString(xox));
 		
-		String[][] arr2D = new String[5][4];
-		System.out.println("The height is "+ arr2D.length);
-		System.out.println("The height is "+ arr2D[0].length);
-		
-		for(int row = 0; row < arr2D.length; row++){
+//		String[][] arr2D = new String[5][4];
+//		System.out.println("The height is "+ arr2D.length);
+//		System.out.println("The height is "+ arr2D[0].length);
+//		
+//		for(int row = 0; row < arr2D.length; row++){
+//			//populate with coordinates
+//			for (int col = 0; col < arr2D[row].length; col++) {
+//				arr2D[row][col] = "(" + row + ", " + col + ")";
+//			}
+//		}
+//		
+//		//print the 2D array
+//		//Every element in a 2D array IS itself an array, so a for-each loop looks like this
+//		for (String[] row : arr2D) {
+//			System.out.println(Arrays.toString(row));
+//		}
+		String[][] pic = new String[10][8];
+		pic[0][3] = "|";
+		pic[1][3] = "O";
+		pic[2][3] = "|";
+		pic[1][2] = "_";
+		pic[1][4] = "_";
+		for(int row = 0; row < pic.length; row++){
 			//populate with coordinates
-			for (int col = 0; col < arr2D[row].length; col++) {
-				arr2D[row][col] = "(" + row + ", " + col + ")";
+			for (int col = 0; col < pic[row].length; col++) {
+				pic[row][col] = " ";
 			}
 		}
-		
-		//print the 2D array
-		//Every element in a 2D array IS itself an array, so a for-each loop looks like this
-		for (String[] row : arr2D) {
-			System.out.println(Arrays.toString(row));
+		for (int row = 5; row < pic.length; row++) {
+			for (int col = 0; col < pic[row].length; col++) {
+				pic[row][col] = "M";
+			}
 		}
+		for (String[] row : pic) {
+			for (String col : row) {
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
