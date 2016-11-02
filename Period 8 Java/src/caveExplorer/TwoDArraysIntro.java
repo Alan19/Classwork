3104cb4e78c435212fd09b8abed3bbc8689059d9
Student8 <Student8@10.8.33.107>
2016-11-02 14:02:34 -0400

    Started Minesweeper Map

--- Period 8 Java/src/caveExplorer/TwoDArraysIntro.java
+++ Period 8 Java/src/caveExplorer/TwoDArraysIntro.java
@@ -1,16 +1,53 @@
 package caveExplorer;
 
-import java.util.Arrays;
-import java.util.function.Function;
-
-import org.omg.CORBA.PUBLIC_MEMBER;
-
 public class TwoDArraysIntro {
 
 	public static void main(String[] args) {
-
+		
+		boolean[][] mines = new boolean[6][6];
+		plantMines(mines);
+		String [][] field = createField(mines);
+		printPic(field);
 	}
 	
+	private static void plantMines(boolean[][] mines) {
+		int numberOfMines = 10;
+		while(numberOfMines > 0){
+			int row = (int)(Math.random() * mines.length);
+			int col = (int)(Math.random() * mines[0].length);
+			if(!mines[row][col]){
+				mines[row][col] = true;
+				numberOfMines--;
+			}
+		}
+		
+	}
+	
+	private static String[][] createField(boolean[][] mines) {
+		String[][] field = new String[mines.length][mines[0].length];
+		for(int row = 0; row < field.length; row++){
+			for (int col = 0; col < field[row].length; col++) {
+				if(mines[row][col]) field[row][col] = "X";
+				else{
+					field[row][col] = countNearby(mines, row, col);
+				}
+			}
+		}
+		
+		return field;
+	}
+
+	private static String countNearby(boolean[][] mines, int row, int col) {
+		for (int r = row - 1; r <= row+1; r++) {
+			for (int c = col; c <= col+1; c++) {
+				if(r >= 0 && r < mines.length && c >= 0 && c < mines[0].length){
+					
+				}
+			}
+		}
+		return null;
+	}
+
 	public static void printPic(String[][] pic){
 //		String[] xox = {"x", "o", "x", "o", "x", "o", "x"};
 //		System.out.println(Arrays.toString(xox));
